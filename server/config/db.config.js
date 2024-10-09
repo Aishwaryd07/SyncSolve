c72ba11e272c8b846af054e19f90cfa6da60494c
Aishwary Dandale <aishdandale7@gmail.com>
2024-10-09 16:44:15 +0530

    migrate to atlas

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_name : "CoalesceDB",
-    DB_url : "mongodb://localhost/CoalesceDB"
+    DB_url : "mongodb+srv://Aishwary:Hireku2@cluster0.7h7bk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 }
